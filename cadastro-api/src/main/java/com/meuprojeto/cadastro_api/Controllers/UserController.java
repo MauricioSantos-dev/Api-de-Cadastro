005c850c1796d94e83a5d57db60d6ebc7b59b600
Mauricio Santos Nascimento Bonfim <09044195@sempreuninassau.com.br>
2025-09-24 10:38:20 -0300

    Controller com rota de cadastro

---
+++ cadastro-api/src/main/java/com/meuprojeto/cadastro_api/Controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.meuprojeto.cadastro_api.Controllers;
+
+
+import com.meuprojeto.cadastro_api.Services.UserService;
+import com.meuprojeto.cadastro_api.Entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping ("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/cadastro")
+    public User CriarUsuario(@RequestBody User user) {
+       return userService.CriarCadastro(user);
+    }
+
+}
--- cadastro-api/src/main/java/com/meuprojeto/cadastro_api/Services/UserService.java
+++ cadastro-api/src/main/java/com/meuprojeto/cadastro_api/Services/UserService.java
@@ -1,7 +1,19 @@
 package com.meuprojeto.cadastro_api.Services;
 
+import com.meuprojeto.cadastro_api.Entities.User;
+import com.meuprojeto.cadastro_api.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public User CriarCadastro(User user) {
+        return userRepository.save(user);
+    }
+
 }
